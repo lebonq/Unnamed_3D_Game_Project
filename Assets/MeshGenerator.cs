6c85a3003714358a98844a212e3dd8f28c57e888
lebonq <lebonquentin@live.fr>
2021-09-09 15:45:27 +0200

    Add mesh generator

--- Assets/MeshGenerator.cs
+++ Assets/MeshGenerator.cs
@@ -4,12 +4,18 @@ using UnityEngine;
 
 public class MeshGenerator : MonoBehaviour
 {
+
+    public int m_size_x;
+    public int m_size_y;
+    public int m_size_z;
+    public int m_nbSegments;
+
     MeshFilter m_Mf;
 
     void Awake()
     {
         m_Mf = GetComponent<MeshFilter>();
-        m_Mf.sharedMesh = GenerateTriangle();
+        m_Mf.sharedMesh = GenerateStripe(new Vector3(m_size_x, m_size_y, m_size_z), m_nbSegments);
     }
 
     Mesh GenerateTriangle()
@@ -33,6 +39,69 @@ public class MeshGenerator : MonoBehaviour
 
         return mesh;
     }
+
+    Mesh GenerateQuad(Vector3 size)
+    {
+        Mesh mesh = new Mesh();
+        mesh.name = "Quad";
+
+        Vector3[] vertices = new Vector3[4];
+        int[] triangles = new int[6];
+
+        vertices[0] = new Vector3(-size.x * .5f, 0, -size.z * .5f);
+        vertices[1] = new Vector3(-size.x * .5f, 0, size.z * .5f);
+        vertices[2] = new Vector3(size.x * .5f, 0, size.z * .5f);
+        vertices[3] = new Vector3(size.x * .5f, 0, -size.z * .5f);
+
+        triangles[0] = 0;
+        triangles[1] = 1;
+        triangles[2] = 2;
+
+        triangles[3] = 0;
+        triangles[4] = 2;
+        triangles[5] = 3;
+
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+
+        return mesh;
+    }
+
+    Mesh GenerateStripe(Vector3 size, int nbSegementX)
+    {
+        Mesh mesh = new Mesh();
+        mesh.name = "Stripe";
+
+        Vector3[] vertices = new Vector3[(nbSegementX + 1)* 2];
+        int[] triangles = new int[nbSegementX * 6]; // 6 car 2 triangles par stripe
+
+        float step_x = size.x /(nbSegementX + 2);
+        int cpt = 0;
+
+        Debug.Log("Vertices");
+        for (int i = 1; i < (nbSegementX + 2); i++)
+        {
+            vertices[cpt] = new Vector3((step_x * i) - size.x * 0.5f, 0,  size.z * 0.5f);
+            vertices[cpt+1] = new Vector3((step_x * i) - size.x * 0.5f, 0, -size.z * 0.5f);
+            cpt += 2;
+        }
+
+        Debug.Log("Triangles");
+        for (int i = 0; i <= (triangles.Length/3)-2; i+=2)
+        {
+            triangles[i*3] = i;
+            triangles[3*i+1] = i+1; 
+            triangles[3*i+2] = i+2;
+
+        }
+
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+
+        return mesh;
+    }
+
+
 
     // Start is called before the first frame update
     void Start()
