29807f9938ad4126507c99e4b18b9d62c93e97f5
lebonq <lebonquentin@live.fr>
2021-09-25 09:15:57 +0200

    Update Player.cs

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -31,11 +31,19 @@ public class Player : MonoBehaviour
         float vInput = Input.GetAxis("Vertical");
         float hInput = Input.GetAxis("Horizontal");
 
-        Vector3 moveVect = transform.forward * vInput * Time.fixedDeltaTime * m_TranslationSpeed;
-        m_Rb.MovePosition(m_Rb.position+moveVect);
-        m_Rb.MoveRotation(Quaternion.AngleAxis(hInput*Time.fixedDeltaTime*m_RotationSpeed,Vector3.up) * transform.rotation);
+        // Vector3 moveVect = transform.forward * vInput * Time.fixedDeltaTime * m_TranslationSpeed;
+        // m_Rb.MovePosition(m_Rb.position+moveVect);
+        // m_Rb.MoveRotation(Quaternion.AngleAxis(hInput*Time.fixedDeltaTime*m_RotationSpeed,Vector3.up) * transform.rotation);
 
-        m_Rb.angularVelocity = Vector3.zero;
+        // m_Rb.angularVelocity = Vector3.zero;
+
+        // Use add force method to change de velocity
+
+        Vector3 velocityDelta = transform.forward * m_TranslationSpeed * vInput - m_Rb.velocity;
+        m_Rb.AddForce(velocityDelta,ForceMode.VelocityChange);
+
+        Vector3 angularVelocityDelta = Vector3.up*m_RotationSpeed*hInput-m_Rb.angularVelocity;
+        m_Rb.AddTorque(angularVelocityDelta, ForceMode.VelocityChange);
     }
 
     private void OnCollisionEnter(Collision collision){
